87774fa17a0d4689f2f77dc8e8e55d8b8fb70bad
hfierroprog <hfierroprog@gmail.com>
2017-09-26 23:36:17 -0300

    Cambios..

--- app/src/main/java/com/example/hbcapps/proyecto_botonera/MainActivity.java
+++ app/src/main/java/com/example/hbcapps/proyecto_botonera/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.hbcapps.proyecto_botonera;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCuchillo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCuchillo = (Button) findViewById(R.id.btncuchillo);
+
+        btnCuchillo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.knife_hitwall1);
+                mp.start();
+            }
+        });
     }
 }
