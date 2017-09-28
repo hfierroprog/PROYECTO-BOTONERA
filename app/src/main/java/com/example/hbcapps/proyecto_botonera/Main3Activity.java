f012473a05a0e6ab7f209b31941ae658c7168ed0
CRaio <32342487+Hastonrez@users.noreply.github.com>
2017-09-28 16:58:20 -0300

    Loquetuquerai

--- app/src/main/java/com/example/hbcapps/proyecto_botonera/Main3Activity.java
+++ app/src/main/java/com/example/hbcapps/proyecto_botonera/Main3Activity.java
@@ -2,13 +2,24 @@ package com.example.hbcapps.proyecto_botonera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
+    private Button Btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        Btn1 = (Button) findViewById(R.id.btn1);
+
+        Btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
